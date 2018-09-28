4ac053a8b051080fec800347a3a7cd2327dc6bab
Jeffrey <toyk1ng@qq.com>
2018-09-28 11:43:18 +0800

    自动化代码版本号调整1

--- app/src/main/java/com/poptek/jenkinstestapp/MainActivity.java
+++ app/src/main/java/com/poptek/jenkinstestapp/MainActivity.java
@@ -1,15 +1,51 @@
 package com.poptek.jenkinstestapp;
 
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        text = findViewById(R.id.test_text);
+        text.setText(packageCode(this)+"-----"+packageName(this));
 
     }
+
+
+    public static int packageCode(Context context) {
+        PackageManager manager = context.getPackageManager();
+        int code = 0;
+        try {
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            code = info.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return code;
+    }
+
+    public static String packageName(Context context) {
+        PackageManager manager = context.getPackageManager();
+        String name = null;
+        try {
+            PackageInfo info = manager.getPackageInfo(context.getPackageName(), 0);
+            name = info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return name;
+    }
+
+
 }
